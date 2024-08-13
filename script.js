1829d7a954152b40b012feee0957ba6dcaf56c47
ALEXANDERkETERLING <alexander_keterling@teilnehmer.cdemy.de>
2024-08-13 13:22:15 +0200

    finish

--- script.js
+++ script.js
@@ -8,13 +8,38 @@ function GetyourGreeting() {
 
     if (age >= 40) {
         if (gender === 'Weiblich') {
-            greeting = `Guten Tag Frau  ${lastName}`;
+            greeting = `Guten  Tag  Frau,  ${lastName}`;
         } else {
-            greeting = `Guten Tag Herr  ${lastName}`;
+            greeting = `Guten  Tag  Herr,  ${lastName}`;
         }
     } else {
-        greeting = `Hallo ${firstName} ${lastName}`;
+        greeting = `Hallo, ${firstName}`;
     }
 
-    document.getElementById('result').innerText = greeting;
+    typeEffect(greeting, 'result');
 }
+
+function typeEffect(text, elementId) {
+    const element = document.getElementById(elementId);
+    
+    const speed = 200; // typing speed in milliseconds
+    let newtext = "";
+
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    (async () => {
+        for (let i = 0; i < text.length; i++) {
+            newtext += text.charAt(i);
+            element.innerText = newtext;
+            await delay(speed);
+        }
+    })();
+}
+
+
+    
+
+
+document.getElementById('greetButton').addEventListener('click', GetyourGreeting);
